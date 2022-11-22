1370d6294ff7b59514e43b0ab281825f4b1d520c
qiangxuhui <qiangxuhui@loongson.cn>
2022-11-22 17:04:00 +0800

    add golang net-loong64

--- vendor/go.etcd.io/bbolt/bolt_loong64.go
+++ vendor/go.etcd.io/bbolt/bolt_loong64.go
@@ -1,9 +1,11 @@
-// +build loong64
-
-package bbolt
+package bolt
 
 // maxMapSize represents the largest mmap size supported by Bolt.
-const maxMapSize = 0x8000000000 // 512GB
+const maxMapSize = 0xFFFFFFFFFFFF // 256TB
 
 // maxAllocSize is the size used when creating array pointers.
 const maxAllocSize = 0x7FFFFFFF
+
+// Are unaligned load/stores broken on this arch?
+var brokenUnaligned = false
+
